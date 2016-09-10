f7ce8c265a6b0da8427de232e827062312f96464
wikki <wikki_1@hotmail.com>
2016-09-10 04:05:22 -0400

    initial android setup

---
+++ app/src/main/java/com/android/pennapps/internetbnb/ShowScreenActivity.java
@@ -0,0 +1,40 @@
+package com.android.pennapps.internetbnb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ShowScreenActivity extends AppCompatActivity {
+
+    Button login;
+    Button signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_screen);
+
+        login = (Button)findViewById(R.id.loginButton);
+        signup = (Button)findViewById(R.id.signupButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ShowScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ShowScreenActivity.this, SignupActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
